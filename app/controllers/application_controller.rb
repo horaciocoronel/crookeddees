2db574e9d6eab901e45613709947fab8daff5dc7
horaciocoronel <arielc@gmail.com>
2017-10-21 12:34:44 -0400

    Setup helper methods in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find(session[:user_id]) if logged_in?
+  end
+
+  helper_method :current_user
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  helper_method :logged_in?
 end
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,4 +7,20 @@ class AppointmentsController < ApplicationController
   def new
     @appointment = Appointment.new
   end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user_id = current_user.id
+    if @appointment.save
+      flash[:notice] = "Your appointment has been saved"
+      redirect_to appointments_path
+    else
+      render :new
+    end
+  end
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:dentist_id, :appointment_at)
+  end
 end
