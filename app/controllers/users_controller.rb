5c484e5b622230b2508238fe11a92b39b5fb41c8
horaciocoronel <arielc@gmail.com>
2017-10-21 11:42:39 -0400

    Users create method + appointments routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Congrats!"
+      session[:user_id] = @user.id
+      redirect_to appointments_url
+    else
+      flash[:alert] = "Please correct mistakes"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :phone)
   end
 end
