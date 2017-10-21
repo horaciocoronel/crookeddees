e0cedcb78494ce8ced17205b522fb5bb30591123
horaciocoronel <arielc@gmail.com>
2017-10-21 12:19:43 -0400

    Create new appointment view, add form to new appointment view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :appointments
+
+  validates :name, :email, :phone, :password, presence:true
+  validates :email, uniqueness: true
 end
